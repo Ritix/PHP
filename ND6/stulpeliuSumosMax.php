bd937134c6248dede7c6fd6479b6c67faebb40fd
Rytis <Rytis.Lekstutis@gmail.com>
2018-02-24 19:38:09 +0200

    ND 5

---
+++ ND6/stulpeliuSumosMax.php
@@ -0,0 +1,10 @@
+<?php
+
+// 3. Turime daugiamatį masyvą, kuris aprašo kažką panašaus į
+//Excel lentelę arba matricą, t.y. pirmas indeksas žymi eilutę, o
+//antras stulpelį. Eilutės gali turėti skirtingą elementų (stulpelių)
+//skaičių. Suskaičiuokite stulpeliuose esančių skaičių sumas ir
+//išveskite didžiausią.
+//
+//testuokite su masyvu:
+//$a = [[1, 3, 4], [2, 5], [2 => 3, 5 => 8], [1, 1, 5 => 1]];
