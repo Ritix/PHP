9aa507be07e89550b64d0c792877b97f0b0df163
Rytis <Rytis.Lekstutis@gmail.com>
2018-02-24 22:11:20 +0200

    ND 5

--- TEST.php
+++ TEST.php
@@ -1,18 +1,2 @@
 <?php
 
-$a = [[1, 2, 3, 4],
-     [5, 6, 7, 8],
-     [9, 10, 11, 12]];
-$ilgis = count($a);
-$s = [];
-
-$max = 0;
-
-for ($i = 0; $i < $ilgis; $i++){
-
-}
-
-
-$myName = "Rytis";
-$myAge = 22;
-echo $myName;
