ce0b0f94dd58c147d5c76b4b91aa42d1ca69fbf5
Rytis <Rytis.Lekstutis@gmail.com>
2018-02-25 15:55:56 +0200

    ND 6

--- ND6/stulpeliuSumosMax.php
+++ ND6/stulpeliuSumosMax.php
@@ -1,10 +1,10 @@
 <?php
 
-$a = [[1, 3, 4], [2, 5], [2 => 3, 5 => 8], [1, 1, 5 => 1]];
+$masyvas = [[1, 3, 4], [2, 5], [2 => 3, 5 => 8], [1, 1, 5 => 1]];
 
 $stulpelis = [];
 
-foreach ($a as $key => $stulpeliai){
+foreach ($masyvas as $key => $stulpeliai){
 
 
     $stulpelis = array_sum($stulpeliai);
@@ -14,3 +14,25 @@ foreach ($a as $key => $stulpeliai){
 
 }
 
+$sumos = [];
+$max_reiksme = 0;
+
+foreach($masyvas as $masyvo_indeksas => $stulpelis) {
+
+    $sumos[$masyvo_indeksas] = 0;
+
+    foreach($stulpelis as $stulpelio_reiksme) {
+        $sumos[$masyvo_indeksas]+=$stulpelio_reiksme;
+    }
+}
+
+foreach($sumos as $suma) {
+    if($suma > $max_reiksme) {
+        $max_reiksme = $suma;
+    }
+}
+
+$max_reiksme = max($sumos);
+
+
+echo 'Max reikšmė: ' .  $max_reiksme.'<p>';
